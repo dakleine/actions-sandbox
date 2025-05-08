8e642a3947a92acc6504dea4b03896b30bbf2d71
Andreas Zulus <zulus@lkw-walter.com>
2025-05-08 09:17:12 +0200

    activate container api

--- main.tf
+++ main.tf
@@ -5,6 +5,16 @@ terraform {
     }
 }
 
+resource "google_project_service" "services" {
+  for_each = toset([
+    "container.googleapis.com",
+  ])
+  project                    = var.project
+  service                    = each.value
+  disable_dependent_services = false
+  disable_on_destroy         = false
+}
+
 provider "google" {
   project = var.project
   region = var.region
