7f6d19e02699b36153c8f637090fac306cebc377
Andreas Zulus <zulus@lkw-walter.com>
2025-05-09 08:19:44 +0200

    dfg

--- kubernetes.tf
+++ kubernetes.tf
@@ -16,7 +16,7 @@ data "terraform_remote_state" "gke" {
 # Configure kubernetes provider with Oauth2 access token.
 # https://registry.terraform.io/providers/hashicorp/google/latest/docs/data-sources/client_config
 # This fetches a new token, which will expire in 1 hour.
-data "google_client_config" "default" {}
+data "google_client_config" "current" {}
 
 data "google_container_cluster" "default" {
   name     = data.terraform_remote_state.gke.outputs.name
