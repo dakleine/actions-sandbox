d67e11f440e0bc0e697019064e145f44d948083f
Andreas Zulus <andreas.zulus@me.com>
2025-05-11 17:29:19 +0200

    secrets.

--- gateway.tf
+++ gateway.tf
@@ -11,12 +11,12 @@ resource "kubernetes_secret" "cert" {
   }
 
   data = {
-    tls.crt = var.gateway-crt
-    tls.key = var.gateway-key
+    "tls.crt" = var.gateway-crt
+    "tls.key" = var.gateway-key
   }
 
   type = "kubernetes.io/tls"
-}
+  }
 resource "kubernetes_manifest" "gateway_gateway_ns_external_gateway" {
   manifest = {
     "apiVersion" = "gateway.networking.k8s.io/v1beta1"
